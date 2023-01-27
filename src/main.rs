d8983d79e6bc7bc00b16a2ceb0fcaa5b6bab969a
William <wmann@uri.edu>
2023-01-27 12:07:30 -0500

    Refactored into different module

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,7 @@
 use std::env::args;
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
-use lazy_static::lazy_static;
-use rumasm::error::Error;
-
-lazy_static! {
-    static ref OPS: std::collections::HashMap<&'static str, (u32, u32)> = {
-        let mut m = std::collections::HashMap::new();
-        m.insert("cmov", (0, 4));
-        m.insert("load", (1, 4));
-        m.insert("store", (2, 4));
-        m.insert("add", (3, 4));
-        m.insert("mul", (4, 4));
-        m.insert("div", (5, 4));
-        m.insert("nand", (6, 4));
-        m.insert("halt", (7, 1));
-        m.insert("map", (8, 3));
-        m.insert("umap", (9, 2));
-        m.insert("out", (10, 2));
-        m.insert("in", (11, 2));
-        m.insert("lp", (12, 3));
-        m.insert("movi", (13, 3));
-        m
-    };
-}
-lazy_static! {
-    static ref REGS: std::collections::HashMap<&'static str, u32> = {
-        let mut m = std::collections::HashMap::new();
-        m.insert("r0", 0);
-        m.insert("r1", 1);
-        m.insert("r2", 2);
-        m.insert("r3", 3);
-        m.insert("r4", 4);
-        m.insert("r5", 5);
-        m.insert("r6", 6);
-        m.insert("r7", 7);
-        m
-    };
-}
-
-#[macro_export]
-macro_rules! MAX_NUM {
-    () => {
-        2_u32.pow(25);
-    };
-}
+use rumasm::parser::parse;
 
 fn main(){
     let input = args().nth(1);
@@ -57,9 +14,9 @@ fn main(){
         std::process::exit(0)
     }
     
-    let bfile = File::open(input.unwrap());
-    let mut file;
-    match bfile{
+    let buffer_file = File::open(input.unwrap());
+    let file;
+    match buffer_file{
         Ok(f) => {
             file = f;
         }
@@ -69,20 +26,22 @@ fn main(){
         }
     }
     
-    let mut out_name = match output{
+    let out_name = match output{
         Some(s) => {s},
-        None => ("out.bin".to_string())
+        None => "out.bin".to_string()
     };
     
-    let mut foo = OpenOptions::new().create(true).write(true).truncate(true).open(out_name).unwrap();
+    let mut instructions = Vec::new();
+    
     let mut line_number = 1;
     for line in BufReader::new(file).lines(){
         let out = parse(line.unwrap());
         
         if out.is_err() {
             let e = out.err().unwrap();
-            eprintln!("{}. Line number: {}", e, line_number);
-            panic!();
+            eprintln!("{} Line number: {}", e, line_number);
+            println!("Assembly failed.");
+            std::process::exit(0);
         }
         
         let p = out.ok().unwrap();
@@ -91,77 +50,20 @@ fn main(){
             None => {}
             Some(inst) =>{
                 let a: [u8; 4] = inst.to_be_bytes();
-                foo.write_all(&a).unwrap();
+                instructions.push(a);
             }
         }
         
         line_number += 1;
     }
-}
-
-fn parse(string: String) -> Result<Option<u32>, String> {
-    if string == ""{
-        return Ok(None);
+    
+    // create an OpenOptions that allows for file creation, writing, and overwriting.
+    let mut foo = OpenOptions::new().create(true).write(true).truncate(true).open(out_name).unwrap();
+    
+    // not sure how to do this better.
+    for word in instructions{
+        foo.write_all(&word).unwrap();
     }
     
-    // split the string into a buffer
-    let mut a = string.split_whitespace();
-    // get the count of the separated words
-    let arg_count = a.clone().count();
-    // collect the args into a vector
-    let mut args: Vec<&str> = a.collect();
-    
-    let op = args[0];
-    
-    if !OPS.contains_key(op){
-        return Err(format!("[ERROR] Invalid operation: {}", op));
-    }
-    
-    let (opcode, argc) = *OPS.get(op).unwrap();
-    
-    if arg_count != argc as usize {
-        return Err(format!("[ERROR] Expected {} arguments!", argc));
-    }
-    
-    let mut instruction = opcode;
-    instruction = instruction << 28;
-
-    if opcode == 13 {
-        let (_, rl) = args[1].split_at(1);
-        let (_, temp) = args[2].split_at(1);
-        let lv = temp.parse::<u32>().unwrap();
-        let rl = rl.replace(",", "");
-        let reg = *REGS.get(&*rl).unwrap();
-        instruction = instruction | (reg << 25) | lv;
-    }
-    else if arg_count == 2{
-        let (_, rc) = args[1].split_at(1);
-        let rc = rc.replace(",", "");
-        let regc = *REGS.get(&*rc).unwrap();
-        instruction = instruction | regc;
-        
-    }
-    else if arg_count == 3{
-        let (_, rb) = args[1].split_at(1);
-        let (_, rc) = args[2].split_at(1);
-        let rb = rb.replace(",", "");
-        let rc = rc.replace(",", "");
-        let regb = *REGS.get(&*rb).unwrap();
-        let regc = *REGS.get(&*rc).unwrap();
-        instruction = instruction | (regb << 4) | regc;
-    }
-    else if arg_count == 4{
-        let (_, ra) = args[1].split_at(1);
-        let (_, rb) = args[2].split_at(1);
-        let (_, rc) = args[3].split_at(1);
-        let ra = ra.replace(",", "");
-        let rb = rb.replace(",", "");
-        let rc = rc.replace(",", "");
-        let rega = *REGS.get(&*ra).unwrap();
-        let regb = *REGS.get(&*rb).unwrap();
-        let regc = *REGS.get(&*rc).unwrap();
-        instruction = instruction | (rega << 8) | (regb << 4) | regc;
-    }
-    
-    return Ok(Some(instruction));
+    println!("Assembly completed. Output file");
 }
---
+++ src/parser.rs
@@ -0,0 +1,162 @@
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref OPS: std::collections::HashMap<&'static str, (u32, u32)> = {
+        let mut m = std::collections::HashMap::new();
+        m.insert("cmov", (0, 4));
+        m.insert("load", (1, 4));
+        m.insert("store", (2, 4));
+        m.insert("add", (3, 4));
+        m.insert("mul", (4, 4));
+        m.insert("div", (5, 4));
+        m.insert("nand", (6, 4));
+        m.insert("halt", (7, 1));
+        m.insert("map", (8, 3));
+        m.insert("umap", (9, 2));
+        m.insert("out", (10, 2));
+        m.insert("in", (11, 2));
+        m.insert("lp", (12, 3));
+        m.insert("movi", (13, 3));
+        m
+    };
+}
+
+#[macro_export]
+macro_rules! max_num {
+    () => {
+        2_u32.pow(25)
+    };
+}
+
+/// Returns an instruction based on the given string.
+pub fn parse(string: String) -> Result<Option<u32>, String> {
+    let a = string.trim();
+    if string == ""{
+        return Ok(None);
+    }
+
+    // split the string into a buffer
+    let a = a.split_whitespace();
+    // get the count of the separated words
+    let arg_count = a.clone().count();
+    // collect the args into a vector
+    let args: Vec<&str> = a.collect();
+
+    let op = args[0];
+
+    // Is the operation found?
+    if !OPS.contains_key(op){
+        return Err(format!("[ERROR] Invalid operation: \"{}\".", op));
+    }
+
+    let (opcode, argc) = *OPS.get(op).unwrap();
+
+    // Check if argc matches expected (This is the same as argc in C, wherein the op is included.)
+    if arg_count != argc as usize {
+        return Err(format!("[ERROR] \"{}\" expects {} arguments!", op, argc - 1));
+    }
+
+    match opcode{
+        7 => { Ok(Some(halt())) }
+        13 => { parse_movi(opcode, args)}
+        n => {
+            println!("{}, {}", n, argc);
+            if argc == 2 { parse_2_arg(opcode, args)}
+            else if argc == 3 { parse_3_arg(opcode, args)}
+            else if argc == 4 { parse_4_arg(opcode, args)}
+            else{
+                Err(format!("[ERROR] Assembler broke REAL BAD. This should never happen as it requires a contradiction"))
+            }
+        }
+    }
+}
+
+fn parse_4_arg(op: u32, args: Vec<&str>) -> Result<Option<u32>, String>{
+    let n1 = args[1].find("r");
+    let n2 = args[2].find("r");
+    let n3 = args[3].find("r");
+
+    if n1 == None || n2 == None || n3 == None {
+        return Err(format!("[ERROR] Register(s) not preceded with 'r'!"));
+    }
+
+    let (_, ra) = args[1].split_at(n1.unwrap() + 1);
+    let (_, rb) = args[2].split_at(n2.unwrap() + 1);
+    let (_, rc) = args[3].split_at(n3.unwrap() + 1);
+    
+    let ra = ra.replace(",", "");
+    let rb = rb.replace(",", "");
+    let rc = rc.replace(",", "");
+    
+    let reg_a = ra.parse::<u32>().unwrap();
+    let reg_b = rb.parse::<u32>().unwrap();
+    let reg_c = rc.parse::<u32>().unwrap();
+    
+    let instruction = op << 28 | (reg_a << 6) | (reg_b << 3) | reg_c;
+    Ok(Some(instruction))
+}
+
+fn parse_3_arg(op: u32, args: Vec<&str>) -> Result<Option<u32>, String>{
+    let n1 = args[1].find("r");
+    let n2 = args[2].find("r");
+
+    if n1 == None || n2 == None {
+        return Err(format!("[ERROR] Register(s) not preceded with 'r'!"));
+    }
+
+    let (_, rb) = args[1].split_at(n1.unwrap() + 1);
+    let (_, rc) = args[2].split_at(n2.unwrap() + 1);
+
+    let rb = rb.replace(",", "");
+    let rc = rc.replace(",", "");
+
+    let reg_b = rb.parse::<u32>().unwrap();
+    let reg_c = rc.parse::<u32>().unwrap();
+
+    let instruction = op << 28 | (reg_b << 3) | reg_c;
+    Ok(Some(instruction))
+}
+
+fn parse_2_arg(op: u32, args: Vec<&str>) -> Result<Option<u32>, String>{
+    let n1 = args[1].find("r");
+
+    if n1 == None {
+        return Err(format!("[ERROR] Register(s) not preceded with 'r'!"));
+    }
+
+    let (_, rc) = args[1].split_at(n1.unwrap() + 1);
+
+    let rc = rc.replace(",", "");
+
+    let reg_c = rc.parse::<u32>().unwrap();
+
+    let instruction = op << 28 | reg_c;
+    Ok(Some(instruction))
+}
+
+fn parse_movi(op: u32, args: Vec<&str>) -> Result<Option<u32>, String>{
+    let n1 = args[1].find("r");
+
+    if n1 == None {
+        return Err(format!("[ERROR] Register not preceded with 'r'!"));
+    }
+
+    let (_, rl) = args[1].split_at(n1.unwrap() + 1);
+    let (_, temp) = args[2].split_at(1);
+    let lv = temp.parse::<u32>().unwrap();
+
+    if lv >= max_num!(){
+        // just in case
+        return Err(format!("[ERROR] Immediate value exceeded 25 bits! value: {}.", lv));
+    }
+
+    let rl = rl.replace(",", "");
+    //let reg = *REGS.get(&*rl).unwrap();
+    let reg = rl.parse::<u32>().unwrap();
+    let instruction = op << 28 | (reg << 25) | lv;
+    Ok(Some(instruction))
+}
+
+fn halt() -> u32{
+    7 << 28
+}
